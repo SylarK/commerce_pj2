795b8aad0811461f6d19f71615f7439298778dba
Lucas Amado <amado.github@gmail.com>
2020-08-05 12:33:45 -0300

    Add data into model Auction

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from auctions.models import *
+
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Comment)
